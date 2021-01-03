90037659e1d97ab3e6fe2109c41fb43479ba3ec5
tomas.hubner@hiq.se <tomas.hubner@hiq.se>
2021-01-03 10:58:07 +0100

    Made the initialization propagate read failures. Added method to get metrics. Fixed bugs in binary search and wrapping of slot index. Tidied up comments.

--- I2C_eeprom_cyclic_store.h
+++ I2C_eeprom_cyclic_store.h
@@ -6,7 +6,6 @@
 // PURPOSE: Supplemental utility class for I2C_EEPROM library
 //
 
-
 #include <I2C_eeprom.h>
 
 /**
@@ -40,192 +39,224 @@
  * **value** members and no constructor/destructor nor other
  * methods/functions - e g a pure DTO.
  */
-template<typename T>
+template <typename T>
 class I2C_eeprom_cyclic_store
 {
-    public:
-        /**
-        * Initializes the instance with the I2C_eeprom object to use
-        * This call searches the eeprom for the latest written version and
-	* sets the current slot accordingly.
-        * 
-        * @param eeprom  the instance of I2C_eeprom to use
-        * @param pageSize is the number of bytes in each write page
-        * @param totalPages specifies the total number of pages to use. By
-	* specifying a number that i sless than the available pages will
-	* exclude the remaining pages from being used.
-        * @return true if initialization succeeds, false otherwise
-        */
-        bool begin(I2C_eeprom & eeprom, uint8_t pageSize, uint16_t totalPages)
+public:
+    /**
+      * @brief Initializes the instance
+      * 
+      * This call searches the eeprom for the latest written version and
+      * sets the current slot accordingly.
+      * 
+      * @param eeprom  The instance of I2C_eeprom to use.
+      * @param pageSize The number of bytes in each write page.
+      * @param totalPages Specifies the total number of pages to use.
+      * Specifying a number that is less than the available pages will
+      * exclude the remaining pages from being used.
+      * @return True if initialization succeeds, false otherwise.
+      */
+    bool begin(I2C_eeprom &eeprom, uint8_t pageSize, uint16_t totalPages)
+    {
+        _eeprom = &eeprom;
+        _pageSize = pageSize;
+        _totalPages = totalPages;
+        auto bufferSize = sizeof(_currentVersion) + sizeof(T);
+        _bufferPages = bufferSize / _pageSize + (bufferSize % _pageSize ? 1 : 0);
+
+        return (_bufferPages < _totalPages) && initialize();
+    };
+
+    /**
+      * @brief Formats the eeprom
+      * 
+      * This must be done if the eeprom already contains data or if the
+	  * structure of the data stored changes.
+      * 
+      * Formatting is done by writing the max version number to each slot,
+      * thus it performs a write cycle to the first write page of each slot. 
+      */
+    void format()
+    {
+        // Reset the EEPROM by writing a ~0 into all pages
+        for (uint16_t slot = 0; slot < _totalPages; slot++)
         {
-            _eeprom = &eeprom;
-            _pageSize = pageSize;
-            _totalPages = totalPages;
-            auto bufferSize = sizeof(uint32_t)+sizeof(T);
-            _bufferPages = bufferSize/_pageSize+(bufferSize%_pageSize ? 1 : 0);
+            _eeprom->writeBlock(slot * _pageSize, (uint8_t *)"\xff\xff\xff\xff", 4);
+        }
 
-            return (_bufferPages < _totalPages) && initialize();
-        };
+        _isEmpty = true;
+        _currentSlot = 0;
+        _isInitialized = true;
+    }
 
-        /**
-         * Formats the eeprom by writing the max version number to each slot 
-         * 
-         * This must be done if the eeprom already contains data or if the
-	 * structure of the data stored changes.
-         */
-        void format()
+    /**
+      * @brief Read data from the eeprom into a buffer.
+      * 
+      * The data is read from the current slot of the eeprom.
+      * 
+      * @param buffer A reference to the buffer to read data into.
+      * @return True if data was read successfully, false otherwise.
+      */
+    bool read(T &buffer) const { return read(&buffer); }
+
+    /**
+      * @brief Read data from the eeprom into a buffer.
+      * 
+      * The data is read from the current slot of the eeprom.
+      * 
+      * @param buffer A pointet to the buffer to read data into.
+      * @return True if data was read successfully, false otherwise.
+      */
+    bool read(T *buffer) const
+    {
+        if (!_isInitialized)
+            return false;
+
+        if (_isEmpty)
+            return false;
+
+        return _eeprom->readBlock((_currentSlot * _bufferPages * _pageSize) + sizeof(_currentVersion), (uint8_t *)buffer, sizeof(T)) == sizeof(T);
+    }
+
+    /**
+      * @brief Write a buffer to the next slot in the eeprom.
+      * 
+      * This updates the current slot of this instance.
+      * 
+      * @param buffer A reference to the buffer to write data from.
+      * @return True if data was written successfully, false otherwise.
+      */
+    bool write(T &buffer) { return write(&buffer); }
+
+    /**
+      * Write data of object to the next slot in the eeprom. This updates
+      * the current slot of the instance.
+      *
+      * @param buffer A pointer to the buffer to write data from.
+      * @return True if data was written successfully, false otherwise.
+      */
+    bool write(T *buffer)
+    {
+        if (!_isInitialized)
         {
-            // Reset the EEPROM by writing a ~0 into all pages
-            for(uint16_t slot = 0; slot < _totalPages; slot++)
-            {
-                _eeprom->writeBlock(slot*_pageSize, (uint8_t *) "\xff\xff\xff\xff", 4);
-            }
+            return false;
+        }
 
+        if (_isEmpty)
+        {
+            _currentSlot = 0;
+            _currentVersion = 0;
+        }
+        else
+        {
+            _currentSlot++;
+            _currentVersion++;
+
+            // Wrap around to start if going past end of alotted region
+            uint16_t maxSlots = _totalPages / _bufferPages;
+            if (_currentSlot >= maxSlots)
+                _currentSlot = 0;
+        }
+
+        auto buffer_length = sizeof(_currentVersion) + sizeof(T);
+        uint8_t tmp[buffer_length];
+
+        memcpy(tmp, &_currentVersion, sizeof(_currentVersion));
+        memcpy(tmp + sizeof(_currentVersion), buffer, sizeof(T));
+
+        auto success = _eeprom->writeBlock(_currentSlot * _bufferPages * _pageSize, tmp, buffer_length) == 0;
+
+        if (success)
+            _isEmpty = false;
+
+        return success;
+    }
+
+    /**
+      * @brief Returns metrics for the eeprom usage.
+      * 
+      * Dividing the returned values of \p writeCounter with \p slots yields the average number of
+      * writes to the individual write pages of the eeprom. This can be used to estimate the remaining
+      * number of possible writes.
+      * 
+      * @param[out] slots The number of slots used to write the data buffer.
+      * @param[out] writeCounter The total number of write to the eeprom since the last format (or first use).
+      * @return True if the instance is initialized and the eeprom is not blank.
+      */
+    bool getMetrics(uint16_t &slots, uint32_t &writeCounter)
+    {
+        if(!_isInitialized || _isEmpty)
+            return false;
+
+        slots = _totalPages / _bufferPages;
+        writeCounter = _currentVersion+1;
+
+        return true;
+    }
+
+private:
+    uint8_t _pageSize;
+    uint16_t _bufferPages;
+    uint16_t _totalPages;
+    uint16_t _currentSlot;
+    uint32_t _currentVersion;
+    bool _isInitialized = false;
+    bool _isEmpty = false;
+    I2C_eeprom *_eeprom;
+
+    bool initialize()
+    {
+        uint16_t startSlot, probeSlot, endSlot;
+        uint32_t current, probe;
+        auto slotSize = _pageSize * _bufferPages;
+
+        startSlot = 0;
+        endSlot = (_totalPages / _bufferPages) - 1;          // Index of last slot
+        probeSlot = startSlot + ((endSlot - startSlot) / 2); // Midway between start and end
+
+        if(_eeprom->readBlock(0, (uint8_t *)&current, sizeof(current)) != sizeof(current))
+        {
+            return false;
+        }
+
+        if (current == ~0UL)
+        {
+            // Memory is blank
             _isEmpty = true;
             _currentSlot = 0;
+            _currentVersion = 0;
             _isInitialized = true;
+            return true;
         }
 
-        /**
-         * Read data from the eeprom into an instance. The data is read from
-	 * the current slot of the eeprom.
-         * 
-         * @param buffer the reference to an object to read data for
-         * @return true if data was read successfully, false otherwise
-         */
-        bool read(T& buffer) const { return read(&buffer); }
-
-        /**
-         * Read data from the eeprom into a memory instance. The data is read
-	 * from the current slot of the eeprom.
-         * @param buffer the instance to read data into
-         * @return true if data was read successfully, false otherwise
-         */
-        bool read(T* buffer) const
+        while (startSlot != probeSlot)
         {
-            if(!_isInitialized)
-                return false;
-
-            if(_isEmpty)
-                return false;
-
-            return _eeprom->readBlock((_currentSlot*_bufferPages*_pageSize)+sizeof(_currentVersion), (uint8_t *) buffer, sizeof(T)) == sizeof(T);
-        }
-
-        /**
-         * Write data of object to the next slot in the eeprom. This updates
-	 * the current slot of this instance.
-         * 
-         * @param buffer the reference to the object to write data for
-         * @return true if data was written successfully, false otherwise
-         */
-        bool write(T& buffer) { return write(&buffer); }
-
-        /**
-         * Write data of object to the next slot in the eeprom. This updates
-	 * the current slot of the instance.
-         *
-         * @param buffer the pointer to the instance to write data for
-         * @return true if data was written successfully, false otherwise
-         */
-        bool write(T* buffer)
-        {
-            if(!_isInitialized)
+            if(_eeprom->readBlock(probeSlot * slotSize, (uint8_t *)&probe, sizeof(current)) != sizeof(current))
             {
                 return false;
             }
 
-            if(_isEmpty)
+            if (probe == ~0UL || probe <= current)
             {
-                _currentSlot = 0;
-                _currentVersion = 0;
+                // 1. Nothing has been written to the memory at Probe
+                // 2. The slots have the same timestamp, this shouldn't happen, treat as if Probe slot hasn't been written
+                // 3. Probe is older that Start, change End to slot before Probe
+                endSlot = probeSlot-1;
             }
             else
             {
-                _currentSlot++;
-                _currentVersion++;
-
-                // Wrap around to start if going past end of alotted region
-                uint16_t maxSlots = _totalPages/_bufferPages;
-                if(_currentSlot > maxSlots)
-                    _currentSlot = 0;
+                // 1. Probe is later than Start, change Start to Probe
+                startSlot = probeSlot;
+                current = probe;
             }
-
-            auto buffer_length = sizeof(_currentVersion)+sizeof(T);
-            uint8_t tmp[buffer_length];
-
-            memcpy(tmp, &_currentVersion, sizeof(_currentVersion));
-            memcpy(tmp+sizeof(_currentVersion), buffer, sizeof(T));
-
-            auto success = _eeprom->writeBlock(_currentSlot*_bufferPages*_pageSize, tmp, buffer_length) == 0;
-
-            if(success)
-                _isEmpty = false;
-
-            return success;
-        }   
-
-    private:
-        uint8_t _pageSize;
-        uint16_t _bufferPages;
-        uint16_t _totalPages;
-        uint16_t _currentSlot;
-        uint32_t _currentVersion;
-        bool _isInitialized = false;
-        bool _isEmpty = false;
-        I2C_eeprom * _eeprom;
-
-        bool initialize()
-        {
-            // Find the current block
-
-            uint16_t startSlot, probeSlot, endSlot;
-            uint32_t current, probe;
-
-            startSlot = 0;
-            endSlot = (_totalPages/_bufferPages)-1; // Index of last slot
-            probeSlot = startSlot+((endSlot-startSlot)/2); // Midway between start and end
-
-            _eeprom->readBlock(0,  (uint8_t *) &current, sizeof(uint32_t));
-
-            if(current == ~0UL)
-            {
-                // Memory is blank
-                _isEmpty = true;
-                _currentSlot = 0;
-                _currentVersion = 0;
-                _isInitialized = true;
-                return true;
-            }
-
-            while(startSlot != probeSlot)
-            {
-                _eeprom->readBlock(probeSlot*_pageSize*_bufferPages, (uint8_t *) &probe, sizeof(uint32_t));
-
-                if(probe == ~0UL || probe <= current)
-                {
-                    // 1. Nothing has been written to the memory at Probe
-                    // 2. The slots have the same timestamp, this shouldn't happen, treat as if Probe slot hasn't been written
-                    // 3. Probe is older that Start, change End to Probe
-                    endSlot = probeSlot;
-                    probeSlot = startSlot+((endSlot-startSlot)/2);
-                    continue;
-                }
-                else if(probe > current)
-                {
-                    // 1. Probe is later than Start. change Start to Probe
-                    startSlot = probeSlot;
-                    probeSlot = startSlot+((endSlot-startSlot)/2);
-                    current = probe;
-                }
-            }
-
-            _currentSlot = startSlot;
-            _currentVersion = current;
-            _isEmpty = false;
-            _isInitialized = true;
-
-            return this->_isInitialized;
+            probeSlot = startSlot + ((endSlot - startSlot + 1) / 2);
         }
-};
 
+        _currentSlot = startSlot;
+        _currentVersion = current;
+        _isEmpty = false;
+        _isInitialized = true;
+
+        return true;
+    }
+};
